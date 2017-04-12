87e2ce2690e08a4c43f12baf9f697c85208491ab
siarhei.miashkou <siarhei.miashkou@leverx.com>
2017-04-12 18:14:38 +0300

    Add some beauty

--- src/Main.java
+++ src/Main.java
@@ -4,33 +4,33 @@ public class Main {
 
     public static void main(String[] args) {
 
-        StringChecker sc = new StringChecker();
+        SymbolInStringSearcher ss = null;
         Scanner in = new Scanner(System.in);
-        char symbol = ' ';
+        char symbol;// = ' ';
         String inputString;
+        boolean searchingSymbolIsGood;
 
         do{
             System.out.print("Input searching symbol: ");
             inputString = in.nextLine();
-            if(inputString.length() != 1){
+            searchingSymbolIsGood = (inputString.length() == 1);
+            if(!searchingSymbolIsGood){
                 System.out.println("Needed only one symbol. Try another one.");
             }
             else{
                 symbol = inputString.toCharArray()[0];
-                sc.setCharToSearch(symbol);
+                ss = new SymbolInStringSearcher(symbol);
             }
         }
-        while (inputString.length() != 1);
+        while (!searchingSymbolIsGood);
 
         System.out.print("Input string: ");
         String s = in.nextLine();
 
         char[] c = s.toCharArray();
 
-        System.out.println("String before symbol: " + sc.readStringBeforeSymbol(s));
-        System.out.println("Chars before symbol: " + sc.readStringBeforeSymbol(c));
-
-
+        System.out.println("String before symbol: " + ss.returnStringBeforeSearchSymbol(s));
+        System.out.println("Chars before symbol: " + ss.returnStringBeforeSearchSymbol(c));
 
 
     }
