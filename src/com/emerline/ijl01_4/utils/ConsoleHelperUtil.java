66c4e7a4ac7f12d62aa4198b82a329fa26ec4c06
siarhei.miashkou <siarhei.miashkou@leverx.com>
2017-04-18 15:49:49 +0300

    Add class for work with console

---
+++ src/com/emerline/ijl01_4/utils/ConsoleHelperUtil.java
@@ -0,0 +1,51 @@
+package com.emerline.ijl01_4.utils;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Created by dzmitry.karayedau on 18-Apr-17.
+ */
+public class ConsoleHelperUtil {
+
+    public static char returnSearchSymbol() {
+        Scanner in = new Scanner(System.in);
+        String inputString;
+        boolean isSymbolAccepted;
+        do {
+            System.out.print("Input searching symbol: ");
+            inputString = in.nextLine();
+            isSymbolAccepted = (inputString.length() == 1);
+            if (!isSymbolAccepted) {
+                System.out.println("Needed only one symbol. Try another one.");
+            }
+        }
+        while (!isSymbolAccepted);
+
+        return inputString.toCharArray()[0];
+    }
+
+    public static String returnInputString() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input string: ");
+        return in.nextLine();
+    }
+
+    public static String returnStringFromFile(String filePath) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader bufferReader = new BufferedReader(new FileReader(filePath))) {
+            int c;
+            System.out.printf("String from file: %s%n", filePath);
+
+            while ((c = bufferReader.read()) != -1) {
+                stringBuilder.append((char) c);
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return stringBuilder.toString();
+    }
+
+}
